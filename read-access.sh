4fb4833cad25f2f086e26c221f16fbf32db99d51
Varsha S <110251631+VarshaRajS@users.noreply.github.com>
2024-06-15 19:49:26 +0530

    Update read-access.sh

--- read-access.sh
+++ read-access.sh
@@ -2,19 +2,19 @@
 
 API_URL = "https://api.github.com" # this is the base url
 
-helper()
-
 USERNAME=$username
 TOKEN=$token
 
 REPO_OWNER=$1
 REPO_NAME=$2
 
+helper()
+
 function github_api_get{
     local endpoint = "$1"
     local url ="${API_URL}/${endpoint}"
 
-    curl -s -u "${REPO_OWNER}:${REPO_NAME}" "url"
+    curl -s -u "${USERNAME}:${TOKEN}" "url"
 }
 
 function list_users_with_read_access{
